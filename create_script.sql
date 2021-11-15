6e4332db101dc22086543370078291e634afda27
Zaikner <79411997+Zaikner@users.noreply.github.com>
2021-11-15 09:39:58 +0100

    Add files via upload

--- create_script.sql
+++ create_script.sql
@@ -1,7 +1,17 @@
-DROP TABLE IF EXISTS DOVODY CASCADE;
+
+DROP TABLE IF EXISTS Series CASCADE;
+CREATE TABLE Series
+(
+   id serial PRIMARY KEY,
+   name varchar not null ,
+   worth double precision not null
+);
+
+DROP TABLE IF EXISTS t_raw_data CASCADE;
 CREATE TABLE t_raw_data
 (
-    Series integer,
+    Timestamp timestamp PRIMARY KEY,
+    Series integer REFERENCES Series,
     Paletts integer,
     Shift integer,
     Next_series integer,
@@ -10,3 +20,21 @@ CREATE TABLE t_raw_data
     Perf_norm_per_min integer,
     Perf_real_per_min integer
 );
+
+DROP TABLE IF EXISTS Event CASCADE;
+CREATE TABLE Event
+(
+    id serial PRIMARY KEY,
+    name varchar not null
+);
+
+DROP TABLE IF EXISTS Events CASCADE;
+CREATE TABLE Events
+(
+    id serial PRIMARY KEY,
+    id_event integer REFERENCES Event,
+    timestamp_begin timestamp not null ,
+    timestamp_end timestamp not null,
+    potencionaly_washed_pallets double precision not null
+);
+
